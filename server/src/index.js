14f20f387178717f9fc4d2ea54628406fd64377f
Jashan Maan <jashanmaan521@gmail.com>
2026-04-04 20:45:26 +0530

    feat: initialize express server with project dependencies, core middleware, and database seeding utilities

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,9 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import connectDb from "./db/db.js";
+import { errorHandling } from "./middlewares/errorHandling.middleware.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -11,6 +13,7 @@ connectDb();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
@@ -18,166 +21,17 @@ app.use(
   }),
 );
 
-import holdingsRoutes from "./routes/holdings.routes.js";
-
-app.use("/api/v1/holdings", holdingsRoutes);
-
+import userRoutes from "./routes/users.routes.js";
 import positionsRoutes from "./routes/positions.routes.js";
-
-app.use("/api/v1/positions", positionsRoutes);
-
+import holdingsRoutes from "./routes/holdings.routes.js";
 import ordersRoutes from "./routes/orders.route.js";
 
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/holdings", holdingsRoutes);
+app.use("/api/v1/positions", positionsRoutes);
 app.use("/api/v1/orders", ordersRoutes);
 
-import userRoutes from "./routes/users.routes.js";
-
-app.use("/api/v1/users", userRoutes);
-
-// app.get("/addHoldings", async (req, res) => {
-//   let tempHoldings = [
-//     {
-//       name: "BHARTIARTL",
-//       qty: 2,
-//       avg: 538.05,
-//       price: 541.15,
-//       net: "+0.58%",
-//       day: "+2.99%",
-//     },
-//     {
-//       name: "HDFCBANK",
-//       qty: 2,
-//       avg: 1383.4,
-//       price: 1522.35,
-//       net: "+10.04%",
-//       day: "+0.11%",
-//     },
-//     {
-//       name: "HINDUNILVR",
-//       qty: 1,
-//       avg: 2335.85,
-//       price: 2417.4,
-//       net: "+3.49%",
-//       day: "+0.21%",
-//     },
-//     {
-//       name: "INFY",
-//       qty: 1,
-//       avg: 1350.5,
-//       price: 1555.45,
-//       net: "+15.18%",
-//       day: "-1.60%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "ITC",
-//       qty: 5,
-//       avg: 202.0,
-//       price: 207.9,
-//       net: "+2.92%",
-//       day: "+0.80%",
-//     },
-//     {
-//       name: "KPITTECH",
-//       qty: 5,
-//       avg: 250.3,
-//       price: 266.45,
-//       net: "+6.45%",
-//       day: "+3.54%",
-//     },
-//     {
-//       name: "M&M",
-//       qty: 2,
-//       avg: 809.9,
-//       price: 779.8,
-//       net: "-3.72%",
-//       day: "-0.01%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "RELIANCE",
-//       qty: 1,
-//       avg: 2193.7,
-//       price: 2112.4,
-//       net: "-3.71%",
-//       day: "+1.44%",
-//     },
-//     {
-//       name: "SBIN",
-//       qty: 4,
-//       avg: 324.35,
-//       price: 430.2,
-//       net: "+32.63%",
-//       day: "-0.34%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "SGBMAY29",
-//       qty: 2,
-//       avg: 4727.0,
-//       price: 4719.0,
-//       net: "-0.17%",
-//       day: "+0.15%",
-//     },
-//     {
-//       name: "TATAPOWER",
-//       qty: 5,
-//       avg: 104.2,
-//       price: 124.15,
-//       net: "+19.15%",
-//       day: "-0.24%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "TCS",
-//       qty: 1,
-//       avg: 3041.7,
-//       price: 3194.8,
-//       net: "+5.03%",
-//       day: "-0.25%",
-//       isLoss: true,
-//     },
-//     {
-//       name: "WIPRO",
-//       qty: 4,
-//       avg: 489.3,
-//       price: 577.75,
-//       net: "+18.08%",
-//       day: "+0.32%",
-//     },
-//   ];
-
-//   await Holdings.insertMany(tempHoldings);
-//   res.send("Holdings added successfully");
-// });
-
-// app.get("/addPositions", async (req, res) => {
-//   let tempPositions = [
-//     {
-//       product: "CNC",
-//       name: "EVEREADY",
-//       qty: 2,
-//       avg: 316.27,
-//       price: 312.35,
-//       net: "+0.58%",
-//       day: "-1.24%",
-//       isLoss: true,
-//     },
-//     {
-//       product: "CNC",
-//       name: "JUBLFOOD",
-//       qty: 1,
-//       avg: 3124.75,
-//       price: 3082.65,
-//       net: "+10.04%",
-//       day: "-1.35%",
-//       isLoss: true,
-//     },
-//   ];
-
-//   await Position.insertMany(tempPositions);
-//   res.send("Positions added successfully");
-// });
+app.use(errorHandling);
 
 app.listen(PORT, () =>
   console.log(`Server running on http://localhost:${PORT}`),
